838741624c71c8ce0b6ce13184823a62a6b14473
willwan92 <wilson3cy@163.com>
2021-01-04 20:42:36 +0800

    添加todo增加上传图片

--- miniprogram/pages/addTodo/addTodo.js
+++ miniprogram/pages/addTodo/addTodo.js
@@ -2,19 +2,64 @@
 const db = wx.cloud.database()
 const todos = db.collection('todos')
 Page({
-
-  onSubmit: function(e) {
-    console.log();
+  data: {
+    image: ''
+  },
+  chooseImage() {
+    wx.chooseImage({
+      count: 1,
+      success: res => {
+        this.setData({ image: res.tempFilePaths[0] })
+      }
+    })
+  },
+  uploadImg() {
+    const img = this.data.image
+    return wx.cloud.uploadFile({
+      filePath: img,
+      cloudPath: Math.floor(Math.random() * 100000000) + img.split('.')[1]
+    }).then(res => {
+      return res.fileID
+    }).catch(err => {
+      wx.showToast({
+        title: err,
+        icon: 'error'
+      })
+    })
+  },
+  addTodo(data) {
     todos
       .add({
-        data: e.detail.value
+        data: data
       })
       .then(res => {
-        wx.showToast({
-          title: '添加成功',
-          icon: 'success'
+        wx.hideLoading({
+          success: (res) => {
+            wx.showToast({
+              title: '添加成功',
+              icon: 'success'
+            })
+            wx.redirectTo({
+              url: '../index/index'
+            })
+          }
         })
       })
+  },
+  onSubmit: function(e) {
+    wx.showLoading({
+      title: '正在添加...',
+    })
 
+    if (this.data.image) {
+      this.uploadImg()
+        .then(res => {
+          const params = { ...e.detail.value }
+          params.image = res
+          this.addTodo(params)
+        })
+    } else {
+      this.addTodo(e.detail.value)
+    }
   }
 })
